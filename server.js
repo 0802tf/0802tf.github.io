8d1379e30057b4c207605a6038efc970341f4e5a
0802tf <0802tf@gmail.com>
2024-08-18 10:48:40 +0900

    reqを使う

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const name = req.query.name;
+  if (name) {
+    res.send(`Hello World! ${name}`);
+  } else {
+    res.send('Hello World!');
+  }
 });
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
