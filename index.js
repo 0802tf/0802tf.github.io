8e8a54286f4af4228eb50f426d09dcc6f738c00a
0802tf <0802tf@gmail.com>
2024-08-20 00:17:59 +0900

    index修正

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
 
 const list = fs.readdirSync('.').filter(value => value.endsWith('.html'));
-const json = JSON.stringify(list);
+const json = JSON.stringify(list, null, 2);
 fs.writeFileSync('./index.json', json, 'utf8');
