626b4af5b6e9aa9c84db6887255ce6ff66e35833
0802tf <0802tf@gmail.com>
2024-09-15 23:35:54 +0900

    mysql導入

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 app.get('/', (req, res) => {
   const name = req.query.name;
   if (name) {
-    res.send(`Hello World! ${name}`);
+    res.json(`Hello World! ${name}`);
   } else {
-    res.send('Hello World!');
+    res.json('Hello World!');
   }
+});
+app.post('/', (req, res) => {
+  const name = req.body.name;
+  const data = req.body.data;
+  res.json({ status: 'ok' });
 });
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
