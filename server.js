59efde1d894d78e35ef2e97e77fed0446e24215d
0802tf <0802tf@gmail.com>
2024-09-19 23:54:02 +0900

    構文修正

--- server.js
+++ server.js
@@ -19,30 +19,12 @@ connection.connect(error => {
 app.use(express.json());
 app.get('/', (req, res) => {
   const name = req.query.name;
-  let sql = '';
-  if (name) {
-    sql = `select data from filelist where name = "${name}";`;
-  } else {
-    sql = 'select name from filelist;'
-  }
-  connection.query(sql, (error, results) => {
-    if (error) {
-      res.status(500).json(error);
-      return;
-    }
-    res.status(200).json(results);
-  });
+  connection.query(name ? `select data from filelist where name = "${name}";` : 'select name from filelist;', (error, results) => res.json(error ? error : results));
 });
 app.post('/', (req, res) => {
   const name = req.body.name;
   const data = req.body.data;
-  connection.query(`insert into filelist values("${name}", "${data}");`, (error, results) => {
-    if (error) {
-      res.status(500).json(error);
-      return;
-    }
-    res.status(200).json(results);
-  });
+  connection.query(`insert into filelist values("${name}", "${data}");`, (error, results) => res.status(500).json(error ? error : results));
 });
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
