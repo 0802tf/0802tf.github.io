dedcc7253de732f456e232db5ea72fa642f04238
0802tf <0802tf@gmail.com>
2025-08-10 16:06:53 +0900

    https修正

--- https.js
+++ https.js
@@ -1,23 +1,29 @@
 import https from 'https';
 
-const zipcode = '8100064';
+const zipcode = '8100042';
 const url = `https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcode}`;
-new Promise((resolve, reject) => https.get(url, res => resolve(res)).on('error', e => reject(e))).then(res => new Promise((resolve, reject) => {
-    if (res.statusCode !== 200) {
-        reject(res.statusCode);
-        return;
-    }
-    let data = '';
-    res.on('data', chunk => data += chunk);
-    res.on('end', () => resolve(data));
-})).then(data => new Promise((resolve, reject) => {
-    try {
-        const parsedData = JSON.parse(data);
-        resolve(parsedData);
-    } catch (e) {
-        reject(e);
-    }
-})).then(parsedData => {
+new Promise((resolve, reject) => {
+    return https.get(url, resolve).on('error', reject);
+}).then(res => {
+    return new Promise((resolve, reject) => {
+        if (res.statusCode !== 200) {
+            reject(res.statusCode);
+            return;
+        }
+        let data = '';
+        res.on('data', chunk => data += chunk);
+        res.on('end', () => resolve(data));
+    });
+}).then(data => {
+    return new Promise((resolve, reject) => {
+        try {
+            const parsedData = JSON.parse(data);
+            resolve(parsedData);
+        } catch (e) {
+            reject(e);
+        }
+    });
+}).then(parsedData => {
     console.log(parsedData);
     console.log(JSON.stringify(parsedData.results[0]));
 }).catch(console.error);
