bb00c04f041f038dacfa8dbb63748730f026e1a7
0802tf <0802tf@gmail.com>
2025-09-10 01:52:09 +0900

    update

--- filesystem.js
+++ filesystem.js
@@ -2,11 +2,11 @@ import fs from 'fs';
 
 try {
     const dir = fs.statSync('.');
-    const file = fs.statSync('./index.txt');
+    const file = fs.statSync('./index.json');
     console.log(dir.isDirectory());
     console.log(file.isDirectory());
     console.log(fs.readdirSync('.'));
-    console.log(fs.readFileSync('./index.txt', 'utf8'));
+    console.log(fs.readFileSync('./index.json', 'utf8'));
     fs.writeFileSync('./test.txt', 'Hello world!', 'utf8');
     fs.appendFileSync('./test.txt', '\nHello overwrite!', 'utf8');
     fs.copyFileSync('./test.txt', './test2.txt');
