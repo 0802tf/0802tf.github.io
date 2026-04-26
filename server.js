91d9f3578e0e406573ade99eb397213bbf07cce2
0802tf <0802tf@gmail.com>
2026-04-27 00:17:29 +0900

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 
 const app = express();
 const port = 3000;
+app.use(express.static('.'));
 app.use(express.json());
 app.get('/', (req, res) => {
   const name = req.query.name;
